e70a62841ce3f130ec9dd015b9ce838ecec09591
alexy-os <agorbunoff.ru@gmail.com>
2025-04-01 18:26:06 +0300

    upd

--- wp-fasty-sf/functions.php
+++ wp-fasty-sf/functions.php
@@ -3,14 +3,14 @@
  * WP Fasty Storefront Child Theme functions
  */
 
-// Отключаем стили родительской темы и подключаем свои
+// Disable parent theme styles and add our own
 function wp_fasty_sf_enqueue_styles() {
-    // Отключаем стили родительской темы
+    // Disable parent theme styles
     wp_dequeue_style('storefront-style');
     wp_dequeue_style('storefront-woocommerce-style');
     wp_dequeue_style('storefront-gutenberg-blocks');
     
-    // Подключаем свои стили
+    // Enqueue our own styles
     wp_enqueue_style(
         'wp-fasty-sf-style',
         get_stylesheet_directory_uri() . '/css/theme.min.css',
@@ -20,25 +20,25 @@ function wp_fasty_sf_enqueue_styles() {
 }
 add_action('wp_enqueue_scripts', 'wp_fasty_sf_enqueue_styles', 20);
 
-// Отключаем инлайн-стили Storefront
+// Disable inline styles
 function wp_fasty_sf_remove_inline_styles() {
     remove_action('wp_enqueue_scripts', 'storefront_add_customizer_css', 130);
 }
 add_action('init', 'wp_fasty_sf_remove_inline_styles');
 
-// Отключаем инлайн-стили Gutenberg блоков
+// Disable inline styles for Gutenberg blocks
 function wp_fasty_sf_remove_gutenberg_inline_styles() {
     wp_dequeue_style('storefront-gutenberg-blocks-inline');
 }
 add_action('wp_enqueue_scripts', 'wp_fasty_sf_remove_gutenberg_inline_styles', 20);
 
-// Отключаем ненужные скрипты Storefront (опционально)
+// Disable unnecessary Storefront scripts (optional)
 function wp_fasty_sf_dequeue_scripts() {
     wp_dequeue_script('storefront-header-cart');
 }
 add_action('wp_enqueue_scripts', 'wp_fasty_sf_dequeue_scripts', 20);
 
-// Добавляем базовые классы для body
+// Add base classes for the body
 function wp_fasty_sf_body_classes($classes) {
     $classes[] = 'font-sans';
     $classes[] = 'bg-background';
