3b385fc75287f6a55124172ff802bc9ba9af3ba0
alexy-os <agorbunoff.ru@gmail.com>
2025-05-01 23:59:14 +0300

    waiver of ux symfony

--- .workspaces/ux-react/src/assets/app.js
+++
@@ -1,4 +0,0 @@
-import { registerReactControllerComponents } from '@symfony/ux-react';
-
-// Регистрируем все React компоненты из директории controllers
-registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
--- .workspaces/ux-react/tools/readme.md
+++
@@ -1,5 +0,0 @@
-Tools for [Latte](https://latte.nette.org)
-==========================================
-
-- [PHP to Latte converter](https://php2latte.nette.org)
-- [Twig to Latte converter](https://twig2latte.nette.org)
