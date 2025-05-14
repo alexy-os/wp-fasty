d472beaa9bbc608e58fd5c09e228e80eec99e42c
alexy-os <agorbunoff.ru@gmail.com>
2025-05-14 16:59:19 +0300

    react to latte/doned!

---
+++ .workspaces/frontend/src/templates/react/distribution/articleTemplate.tsx
@@ -0,0 +1,19 @@
+const ArticleTemplate = ({ posts }: any) => {
+
+  return (
+    <>
+      {posts.map((post: any) => <article key={post.id} className="bg-card text-card-foreground flex flex-col gap-4 rounded-xl border shadow-sm">
+        <header className="flex flex-col gap-2 px-6 pt-6">
+          <h2 className="text-2xl font-semibold tracking-tight">{post.title}</h2>
+          {post.date && <div className="flex flex-wrap items-center gap-3 text-sm text-muted-foreground">
+            <time dateTime={post.date.formatted} className="text-sm text-muted-foreground">{post.date.display}</time>
+          </div>}
+        </header>
+        {post.excerpt && <div className="max-w-none px-6 py-4">
+          <p>{post.excerpt}</p>
+        </div>}
+      </article>)}
+    </>
+  );
+};
+export const Article = ArticleTemplate;
